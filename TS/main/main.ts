e57acd082fc218a9cddcab1f3c0202df3d0b4f49
RomanRanniew <roman.ranniew@tdsoft.pl>
2016-12-24 19:24:36 +0100

    ts lint

--- TS/main/main.ts
+++ TS/main/main.ts
@@ -1,8 +1,1 @@
 import "../tools/utils";
-const s = undefinedFun();
-wrongjson : {
-"test : ";string;",;;;;;;;
-"var"; : "val";
-};
-let g = 5;
-let i = 60;
--- bin/tslint.js
+++ bin/tslint.js
@@ -1,13 +1,19 @@
 require("./base.js")();
-var tslint = require("tslint");
-var Linter = require("tslint/lib/linter");
-var configuration_1 = require("tslint/lib/configuration");
+const tslint = require("tslint");
+const Linter = require("tslint/lib/linter");
+const config_O = require("tslint/lib/configuration");
 
+let args = process.argv.slice(2);
+let prod_i = args.indexOf("prod");
+let prod_temp = args[prod_i+1];
+
+const prod = typeof(prod_temp) === "undefined" ? false : prod_temp;
 const fileName = "TS/**/*.*";
 const configuration = {
 	rules: {
 		"no-var-requires": false,
 		"variable-name": true,
+		"no-console": [false],
 		"quotemark": [true, "double"]
 	},
 	"rulesDirectory": ["node_modules/tslint/lib/rules"]
@@ -17,36 +23,52 @@ const options = {
 	rulesDirectory: "/node_modules/tslint/lib/rules/",
 	formattersDirectory: "/node_modules/tslint/lib/formatters/"
 };
-const program = Linter.createProgram("../tsconfig.json", "");
+
+const program = Linter.createProgram("ts_program.json", "");
 const dfiles = Linter.getFileNames(program);
-var linter = new Linter(options, program);
-var lint_A = program.getSemanticDiagnostics();
-
-if (lint_A.length > 0) {
-	beep(2);
-}
-
+const linter = new Linter(options, program);
+const lint_A = program.getSemanticDiagnostics();
 
 glob(fileName, function (er, files) {
+	let configLoad = "";
+	let file_contents = "";
 	files.forEach(file => {
-		var dest_file = file.substring(file.lastIndexOf("/") + 1, file.lastIndexOf("."));
-		var file_contents = fs.readFileSync(file, "utf8");
-		var result = linter.lint("utils.ts", "TS/tools/", configuration);
-		var configLoad = configuration_1.findConfiguration(null, file);
+		file_contents = fs.readFileSync(file, "utf8");
+		if( prod ){
+			console.log("loading prod cofiguration...");
+			configLoad = config_O.findConfiguration("./bin/tsconfig_prod.json", file);
+		}else{
+			configLoad = config_O.findConfiguration("./bin/tsconfig_dev.json", file);
+		}
 		linter.lint(file, file_contents, configLoad.results);
 	});
-	var lintResult = linter.getResult();
-	console.log(lintResult);
-	var res =  util.inspect(lintResult);
-	fs.writeFileSync("jslint.log", res, { encoding: "utf-8" });
+	const lintResult = linter.getResult();
+
+	if( lintResult.failureCount > 0){
+		beep(2);
+		console.log( ("Errors count: "+lintResult.failureCount+" x").red );
+		for( let i in lintResult.failures){
+			let res =  lintResult.failures[i];
+			console.log( ("Filename: "+res.fileName+"\n"+res.failure).yellow );
+			console.log( "starting "+("line:"+res.startPosition.lineAndCharacter.line+" pos:"+ res.startPosition.lineAndCharacter.character).bold.cyan );
+			console.log( "ending "+("line:"+res.endPosition.lineAndCharacter.line+" pos:"+ res.endPosition.lineAndCharacter.character+"\n").bold.cyan );
+			if( typeof(res.fix) !== "undefined" ){
+				console.log( "Possible fix:"+res.fix );
+			}
+		}
+		fs.writeFileSync("jslint.log", util.inspect( lintResult.failures ), { encoding: "utf-8" });
+		return 0;
+	}else{
+		console.log( ("No errors! ✔").green );
+	}
 });
 
-//linter.lint(file, contents, configLoad.results);
-
-return 0;
-for (i in lint_A) {
-	var filename = lint_A[i].file.fileName.split("\\");
-	filename = filename.splice((filename.length - 2), 2).join("/");
-	var text = "Message: " + lint_A[i].messageText.yellow + " \n > " + lint_A[i].file.text + "\n filename: " + filename.green + "\n";
-	console.log(text);
+if (lint_A.length > 0) {
+	console.log("Semantics test: \n".bgGreen.inverse);
+	for (let i in lint_A) {
+		let filename = lint_A[i].file.fileName.split("\\");
+		filename = filename.splice((filename.length - 2), 2).join("/");
+		let text = "filename: " + filename.green + "\n Message: " + lint_A[i].messageText.yellow + " \n > " + lint_A[i].file.text;
+		console.log(text);
+	}
 }
